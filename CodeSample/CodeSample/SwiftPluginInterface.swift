764526b75e0b6d8d9f8ff699dfef6cabdda967b0
Nisar Pattasseri <nisar.pv@exilant.com>
2014-11-19 01:16:25 +0530

    Added framework files

---
+++ CodeSample/CodeSample/SwiftPluginInterface.swift
@@ -0,0 +1,9 @@
+//
+//  SwiftPluginInterface.swift
+//  CodeSample
+//
+//  Created by Nisar Pattasseri on 14/11/14.
+//  Copyright (c) 2014 Nisco. All rights reserved.
+//
+
+import Foundation
---
+++ CodeSample/Plugins/SwiftAppPlugin/SwiftAppPlugin/SwiftAppPlugin.h
@@ -0,0 +1,19 @@
+//
+//  SwiftAppPlugin.h
+//  SwiftAppPlugin
+//
+//  Created by Nisar Pattasseri on 14/11/14.
+//  Copyright (c) 2014 Nisco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftAppPlugin.
+FOUNDATION_EXPORT double SwiftAppPluginVersionNumber;
+
+//! Project version string for SwiftAppPlugin.
+FOUNDATION_EXPORT const unsigned char SwiftAppPluginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftAppPlugin/PublicHeader.h>
+
+
